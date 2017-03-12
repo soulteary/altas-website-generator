672b01c256f7a3f7c914bf5cc39465f835cdcb70
soulteary <suyang.sy@alibaba-inc.com>
2017-03-12 20:23:29 +0800

    调整构建文件，先low一下

--- bin/release.sh
+++ bin/release.sh
@@ -16,7 +16,7 @@ cp /etc/nginx/nginx.conf public/nginx.conf
 
 git clone --depth 1 --single-branch --branch dev git@github.com:alibaba/atlas.git
 
-cp -r atlas/atlas-docs/_book public/docs
+cp -r atlas/atlas-docs/_book/ public/docs
 rm -rf atlas
 
 node ./bin/updateChangelogHash.js
