bf8404fc373428eedd20e2dc993833ab1ee565e1
soulteary <suyang.sy@alibaba-inc.com>
2017-03-12 20:05:58 +0800

    自动从atlas主仓库拉取文档

--- bin/release.sh
+++ bin/release.sh
@@ -14,4 +14,9 @@ cp -r ./website/* public/
 cp -r ./Dockerfile-deploy public/Dockerfile
 cp /etc/nginx/nginx.conf public/nginx.conf
 
+git clone --depth 1 --single-branch --branch dev git@github.com:alibaba/atlas.git
+
+cp -r atlas/atlas-docs/_book public/docs
+rm -rf atlas
+
 node ./bin/updateChangelogHash.js
--- bin/scripts/release.sh
+++ bin/scripts/release.sh
@@ -6,6 +6,7 @@ mkdir -p $BASE_DIR/public
 
 echo docker run --rm -it -v $BASE_DIR/posts:/data/posts:ro -v $BASE_DIR/public:/data/public:rw atlas/website-builder bin/release.sh
 
-docker run --rm -it -v $BASE_DIR/posts:/data/posts:ro -v $BASE_DIR/public:/data/public:rw atlas/website-builder bin/release.sh
+#docker run --rm -it -v $BASE_DIR/posts:/data/posts:ro -v $BASE_DIR/public:/data/public:rw atlas/website-builder bin/release.sh
+docker run --rm -it -v $BASE_DIR/posts:/data/posts:ro -v $BASE_DIR/public:/data/public:rw -v /Users/$USER/.ssh:/root/.ssh:ro -v /Users/$USER/.gitconfig:/root/.gitconfig:ro atlas/website-builder bin/release.sh
 
 #cp -r $BASE_DIR/posts/book $BASE_DIR/public
